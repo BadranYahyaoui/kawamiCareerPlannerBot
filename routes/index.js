f79f23eaade0d060b3ccf8f1f7d8ca9fd61e8645
badran08 <mohamedbadran.yahyaoui@esprit.tn>
2017-03-07 18:23:08 +0100

    adding template assets

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ var paramlist=[];
 router.get('/', function(req, res, next) {
     res.render('index.twig', { params: paramlist});
 });
+router.get('/autotentification', function(req, res, next) {
+    res.render('autotentification.twig', { params: paramlist});
+});
 
 module.exports = router;
