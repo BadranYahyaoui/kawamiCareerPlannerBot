4119d359fc92625bd9d067699b0617be00770daa
Raynzus <nader.kasri@esprit.tn>
2017-04-26 08:21:00 +0100

    Added frontoffice

---
+++ public/frontoffice/services/tagservice.js
@@ -0,0 +1,16 @@
+var services = angular.module('bot.services', ['ngResource']);
+
+services.factory('TagsFactory', function ($resource) {
+    return $resource('https://carrerplanner-bot.herokuapp.com/api/tags', {}, {
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+    })
+});
+
+services.factory('TagFactory', function ($resource) {
+    return $resource('https://carrerplanner-bot.herokuapp.com/api/tags/:id', {id:'@_id'}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT'},
+        delete: { method: 'DELETE' }
+    })
+});
