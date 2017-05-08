74afc19878fb3832fedd73fdb159365f3a42c2b8
marwadenden <marwa.denden@esprit.tn>
2017-05-09 00:45:50 +0200

    Update vocationalservice.js

--- public/angularadmin/services/vocationalservice.js
+++ public/angularadmin/services/vocationalservice.js
@@ -1,7 +1,8 @@
 services.factory('VocationalsFactory', function ($resource) {
-    return $resource('https://carrerplanner-bot.herokuapp.com/api/vocationals', {}, {
-        query: { method: 'GET', isArray: true },
-        create: { method: 'POST' }
+    return $resource('http://localhost:3000/api/vocationals/:id', {id:'@_id'}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT'},
+        delete: { method: 'DELETE' }
     })
 });
 
