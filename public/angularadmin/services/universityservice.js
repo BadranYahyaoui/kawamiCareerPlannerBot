e7a3040ad9e83f9d060e80e234956a1196e1affd
marwadenden <marwa.denden@esprit.tn>
2017-05-09 00:45:05 +0200

    Update universityservice.js

--- public/angularadmin/services/universityservice.js
+++ public/angularadmin/services/universityservice.js
@@ -1,7 +1,19 @@
+//var services = angular.module('bot.services', ['ngResource']);
 services.factory('UniversitiesFactory', function ($resource) {
-    return $resource('https://carrerplanner-bot.herokuapp.com/api/universities', {}, {
+    return $resource('http://localhost:3000/api/universities',{}, {
         query: { method: 'GET', isArray: true },
         create: { method: 'POST' }
     })
+
 });
+services.factory('UniversitieFactory', function ($resource) {
+    return $resource('http://localhost:3000/api/universities/:id', {id:'@_id'}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT'},
+        delete: { method: 'DELETE' }
+    })
+   
+});
+
+
 
